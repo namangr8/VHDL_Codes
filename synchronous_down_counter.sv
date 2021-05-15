404f8f416492739c0105c340f2d6c25f9b87040a
Naman Gupta <76123992+namangr8@users.noreply.github.com>
2021-05-15 16:37:15 +0530

    Create synchronous_down_counter.sv

---
+++ synchronous_down_counter.sv
@@ -0,0 +1,30 @@
+module syncdown(output reg[3:0] count,input clk,rst);
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        count<=15;
+      else if(clk)
+        count<=count-1;
+       else
+        count<=15;
+    end 
+endmodule
+
+/////////////////////////////////////////////////////////////////////////
+
+module syncdown_tb();
+  reg clk,rst;
+  wire[3:0] count;
+  syncdown g1(count,clk,rst);
+  
+  initial begin
+    $dumpfile("syncdown_tb.vcd");
+    $dumpvars(0,syncdown_tb);
+    clk=0;rst=1;
+    #20 rst=0;
+    #150 $stop;
+  end
+  always #5 clk=~clk;
+endmodule
+  
+///////////////////////////////////////////////////////////////////////
